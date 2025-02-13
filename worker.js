387c5ac9b911aad934371bfd3669535d45d73480
Simon Heimlicher <simon.github@heimlicher.com>
2025-02-13 21:05:16 +0100

    feat: add CORS header also to non-static paths

--- worker.js
+++ worker.js
@@ -1,4 +1,3 @@
-// const API_HOST = "app.posthog.com" // Change to "eu.posthog.com" for the EU region
 const API_HOST = "eu.posthog.com"; // Change to "eu.posthog.com" for the EU region
 
 async function handleRequest(event) {
@@ -7,24 +6,22 @@ async function handleRequest(event) {
     const search = url.search;
     const pathWithParams = pathname + search;
 
+    let response;
     if (pathname.startsWith("/static/")) {
-        return retrieveStatic(event, pathWithParams);
+        response = await retrieveStatic(event, pathWithParams);
     } else {
-        return forwardRequest(event, pathWithParams);
+        response = await forwardRequest(event, pathWithParams);
     }
+
+    // Ensure CORS headers are applied to ALL responses
+    return addCORSHeaders(response);
 }
 
 async function retrieveStatic(event, pathname) {
     let response = await caches.default.match(event.request);
     if (!response) {
         response = await fetch(`https://${API_HOST}${pathname}`);
-        // Clone the response so we can modify headers
         response = new Response(response.body, response);
-
-        // Set CORS headers
-        response.headers.set("Access-Control-Allow-Origin", "*");
-        response.headers.set("Access-Control-Allow-Methods", "GET, OPTIONS");
-        response.headers.set("Access-Control-Allow-Headers", "Content-Type");
 
         // Set Cache-Control header for static assets
         response.headers.set("Cache-Control", "public, max-age=2592000"); // 30 days
@@ -38,7 +35,17 @@ async function retrieveStatic(event, pathname) {
 async function forwardRequest(event, pathWithSearch) {
     const request = new Request(event.request);
     request.headers.delete("cookie");
-    return await fetch(`https://${API_HOST}${pathWithSearch}`, request);
+    let response = await fetch(`https://${API_HOST}${pathWithSearch}`, request);
+    return response;
+}
+
+// Function to add CORS headers
+function addCORSHeaders(response) {
+    let newResponse = new Response(response.body, response);
+    newResponse.headers.set("Access-Control-Allow-Origin", "*");
+    newResponse.headers.set("Access-Control-Allow-Methods", "GET, OPTIONS");
+    newResponse.headers.set("Access-Control-Allow-Headers", "Content-Type");
+    return newResponse;
 }
 
 addEventListener("fetch", (event) => {
